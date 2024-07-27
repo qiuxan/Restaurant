66632823adc883f7cc44d43063400ec13ed4f135
qiuxan <qiuxan@gmail.com>
2024-07-27 17:42:41 +1000

    8 Create and register a Service

--- Restaurant.API/Controllers/WeatherForecastController.cs
+++ Restaurant.API/Controllers/WeatherForecastController.cs
@@ -6,27 +6,22 @@ namespace Restaurant.API.Controllers;
 [Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+ 
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly WeatherForecastService _weatherForecastService;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger , WeatherForecastService weatherForecastService)
     {
         _logger = logger;
+        _weatherForecastService = weatherForecastService;
     }
 
     [HttpGet]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var result = _weatherForecastService.Get();
+        return result;
     }
 }
--- Restaurant.API/Program.cs
+++ Restaurant.API/Program.cs
@@ -1,8 +1,14 @@
+using Restaurant.API.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Register WeatherForecastService
+builder.Services.AddScoped<WeatherForecastService>();
+
 
 var app = builder.Build();
 
