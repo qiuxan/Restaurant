00b6d382284f07559f323f0ea636f17c68aefc49
qiuxan <qiuxan@gmail.com>
2024-07-28 15:49:57 +1000

    play around the route and query with controller

--- Restaurant.API/Controllers/WeatherForecastController.cs
+++ Restaurant.API/Controllers/WeatherForecastController.cs
@@ -6,20 +6,20 @@ namespace Restaurant.API.Controllers;
 [Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
- 
+
 
     private readonly ILogger<WeatherForecastController> _logger;
 
     private readonly IWeatherForecastService _weatherForecastService;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger , IWeatherForecastService weatherForecastService)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
     {
         _logger = logger;
         _weatherForecastService = weatherForecastService;
     }
 
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("{take}/allweather")]
+    public IEnumerable<WeatherForecast> Get([FromQuery] int max, [FromRoute] int take)
     {
         var result = _weatherForecastService.Get();
         return result;
