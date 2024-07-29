0b6b0161dafd9e361914f03c7a1aaad2310d307e
qiuxan <qiuxan@gmail.com>
2024-07-29 16:35:47 +1000

    install and setup EntityFramework for SQL server

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
+
+        public int RestaurantId { get; set; }
     }
 }
