beda792e7fe2d66c8698d92cd297d41c90967144
qiuxan <qiuxan@gmail.com>
2024-07-30 14:47:20 +1000

    add New Column to table

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -7,6 +7,7 @@
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
 
+        public int? KiloCalories { get; set; }
         public int RestaurantId { get; set; }
     }
 }
