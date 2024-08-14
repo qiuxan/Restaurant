2459f2b2b01ad4af4b1b69322ac2bd0f0762fb3e
qiuxan <qiuxan@gmail.com>
2024-08-14 11:51:15 +1000

    Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿
+using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator: AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowPageSize = [5,10,15,30];
+    public GetAllRestaurantsQueryValidator()
+    { 
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1).WithMessage("Page number must be greater than or equal to 1");
+
+        RuleFor(x => x.PageSize)
+            .Must(x => allowPageSize.Contains(x)).WithMessage($"Page size must be [{string.Join(",", allowPageSize)}]")
+
+    }   
+}
